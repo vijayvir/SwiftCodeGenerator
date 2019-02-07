a05f2a6795612c5200537edfa1b6a5ea3619430a
vijayvir <virvijay37@gmail.com>
2019-02-07 12:02:04 +0530

    c
    c

--- LeoSwiftCoder.swift
+++ LeoSwiftCoder.swift
@@ -12,7 +12,7 @@ class LeoSwiftCoder {
     func leoClassMake(withName : String , json :Any) {
         switch json.self {
         case is [String : Any] :
-
+            
             if let someJson = json as? [String : Any] {
                 print("class \(withName) {")
                 print("var serverData : [String: Any] = [:]")
@@ -30,7 +30,7 @@ class LeoSwiftCoder {
                 print("}")
                 for object in someJson.keys {
                     if let nextObject = someJson[object] as? [String : Any] {
-                        self.leoClassMake(withName: object, json: nextObject)
+                        self.leoClassMake(withName: object.capitalized, json: nextObject)
                     } else if let nextObject = someJson[object] as? [[String : Any]] {
                         if nextObject.count > 0 {
                             self.leoClassMake(withName: object.capitalized, json: nextObject.first!)
@@ -59,19 +59,28 @@ class LeoSwiftCoder {
     func defineVariable(key :String, send :Any) -> String{
         switch send.self {
         case is [String : Any] :
-            return  "if let \(key) = dict[\"\(key)\"] as? \(key.capitalized) { \n self.\(key) = \(key) \n }"
+            
+            let some =  """
+            if let object = dict[\"\(key)\"] as? [String : Any] {
+            let some =  \(key.capitalized)(dict: object)
+            self.\(key) = some }
+            """
+            
+            return  some
+            
+        //"if let \(key) = dict[\"\(key)\"] as? \(key.capitalized) { \n self.\(key) = \(key) \n }"
         case is [[String : Any]] :
             
-       let some =  """
-       if let \(key) = dict[\"\(key)\"] as? [[String : Any]] {
-             self.\(key) = []
-           for object in \(key) {
-                    let some =  \(key.capitalized)(dict: object)
-                     self.\(key)?.append(some)
-         
+            let some =  """
+            if let \(key) = dict[\"\(key)\"] as? [[String : Any]] {
+            self.\(key) = []
+            for object in \(key) {
+            let some =  \(key.capitalized)(dict: object)
+            self.\(key)?.append(some)
+            
             }
-       }
-    """
+            }
+            """
             return  some
         case is Int :
             return "if let \(key) = dict[\"\(key)\"] as? Int { \n self.\(key) = \(key) \n }"
