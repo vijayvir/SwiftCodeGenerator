fab4871b53e2ad5e3a4a3abd903b395172de3e25
vijayvir <virvijay37@gmail.com>
2019-04-16 14:41:03 +0530

    de

--- LeoSwiftCoder.swift
+++ LeoSwiftCoder.swift
@@ -32,28 +32,10 @@ class LeoSwiftCoder {
                 print("class \(withName) {")
                 print("var serverData : [String: Any] = [:]")
                 for key in someJson.keys {
-                    
-                    let type = self.typeOf(send: someJson[key]! , key : key)
-                    
-                    if type == "[Bool]" {
-                        
-                        print( "var \(key.unCapitalizedLSC) : \(type)? = []" )
-                    }else if  type == "[Int]" {
-                        
-                        print( "var \(key.unCapitalizedLSC) : \(type)? = [] " )
-                    }else if  type == "[String]" {
-                        
-                        print( "var \(key.unCapitalizedLSC) : \(type)? = [] " )
-                    }else {
-                        
-                        print( "var \(key.unCapitalizedLSC) : \(type)?" )
-                    }
-                    
-                    
+                    print( "var \(key.unCapitalizedLSC) : \(self.typeOf(send: someJson[key]! , key : key))?" )
                 }
                 print("init(dict: [String: Any]){")
                 print(" self.serverData = dict \n ")
-                
                 for key in someJson.keys {
                     print( self.defineVariable(key :key , send: someJson[key]!) )
                     
@@ -70,11 +52,6 @@ class LeoSwiftCoder {
                     }else if let nextObject = someJson[object] as? Array<String> {
                         self.leoClassMake(withName: object.capitalized, json: nextObject)
                     }
-                    else if let nextObject = someJson[object] as? Array<Int> {
-                        self.leoClassMake(withName: object.capitalized, json: nextObject)
-                    }else if let nextObject = someJson[object] as? Array<Bool> {
-                        self.leoClassMake(withName: object.capitalized, json: nextObject)
-                    }
                     
                 }
                 print("}")
@@ -84,9 +61,9 @@ class LeoSwiftCoder {
             if let someJson = json as? [String] {
                 if someJson.count > 0 {
                     print("class \(withName) {")
-                    print("var serverData : [String] = []")
+                    print("var serverData : String = \"\"")
                     
-                    print("init(dict: [String]){")
+                    print("init(dict: String){")
                     print(" self.serverData = dict \n ")
                     
                     
@@ -94,39 +71,12 @@ class LeoSwiftCoder {
                     print("}")
                 }
             }
-        case is Array<Int> :
-            if let someJson = json as? [Int] {
-                if someJson.count > 0 {
-                    print("class \(withName) {")
-                    print("var serverData : [Int] = []")
-                    print("init(dict: [Int] ){")
-                    print(" self.serverData = dict \n ")
-                    print("}")
-                    print("}")
-                }
-            }
-        case is Array<Bool> :
-            if let someJson = json as? [Bool] {
-                if someJson.count > 0 {
-                    print("class \(withName) {")
-                    print("var serverData : [Bool] = []")
-                    print("init(dict: [Bool] ){")
-                    print(" self.serverData = dict \n ")
-                    print("}")
-                    print("}")
-                }
-            }
+            
+            
         case is Int :
             print("")
-            
-            
-            
         case is Array<Any> :
             print("")
-            
-            
-            
-            
         case is Dictionary<String, Any> :
             print("")
         case is Bool :
@@ -168,27 +118,23 @@ class LeoSwiftCoder {
             return  some
         case is Int :
             return "if let \(key.unCapitalizedLSC) = dict[\"\(key)\"] as? Int { \n self.\(key.unCapitalizedLSC) = \(key.unCapitalizedLSC) \n }"
+            
+        case is Double :
+            return "if let \(key.unCapitalizedLSC) = dict[\"\(key)\"] as? Double { \n self.\(key.unCapitalizedLSC) = \(key.unCapitalizedLSC) \n }"
+            
         case is Array<String>  :
             
             let some =  """
-            if let \(key.unCapitalizedLSC) = dict[\"\(key)\"] as? [String] {
-            self.\(key.unCapitalizedLSC) = \(key.unCapitalizedLSC)
-            }
-            """
-            return  some
-        case is Array<Int>  :
+            if let \(key.unCapitalizedLSC) = dict[\"\(key)\"] as? [[String]] {
+            self.\(key.unCapitalizedLSC) = []
             
-            let some =  """
-            if let \(key.unCapitalizedLSC) = dict[\"\(key)\"] as? [Int] {
-            self.\(key.unCapitalizedLSC) = \(key.unCapitalizedLSC)
-            }
-            """
-            return  some
-        case is Array<Bool>  :
+            if \(key.unCapitalizedLSC).count > 0 {
+            for object in \(key.unCapitalizedLSC).first! {
+            let some =  \(key.capitalized)(dict: object)
+            self.\(key.unCapitalizedLSC)?.append(some)
             
-            let some =  """
-            if let \(key.unCapitalizedLSC) = dict[\"\(key)\"] as? [Bool] {
-            self.\(key.unCapitalizedLSC) = \(key.unCapitalizedLSC)
+            }
+            }
             }
             """
             return  some
@@ -222,19 +168,24 @@ class LeoSwiftCoder {
                 return "[[String : Any]]"
             }
         case is Array<String> :
-            return "[String]"
-        case is Array<Int> :
-            return "[Int]"
-        case is Array<Bool> :
-            return "[Bool]"
+            if (key != nil) {
+                return "[\(key!.capitalized)]"
+            } else {
+                return "[[String]]"
+            }
+            
         case is [[String]] :
-            return "[[String]]"
-        case is [[Bool]] :
-            return "[[Bool]]"
-        case is [[Int]] :
-            return "[[Int]]"
+            if (key != nil) {
+                return "[\(key!.capitalized)]"
+            } else {
+                return "[[String]]"
+            }
         case is Int :
             return "Int"
+        case is Double :
+            return "Double"
+            
+            
         case is Array<Any> :
             return "Array<Any>"
         case is Dictionary<String, Any> :
